352132a04957de0c936767f68c7b77ff9ac8f840
Jimmy Godin <jimmy.godin@hotmail.com>
2018-04-20 09:22:43 +0200

    tweede commit

--- src/be/vdab/singleton/Main.java
+++ src/be/vdab/singleton/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Versnellingsbak.INSTANCE.schakelOmhoog();
 		Versnellingsbak.INSTANCE.schakelOmlaag();
+		Versnellingsbak.INSTANCE.schakelOmlaag();
 	}
 
 }
